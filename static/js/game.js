11559ed201f4976cb5ee8adc7e12432a2a5b6d0c
Andras Molnar <andris9016@gmail.com>
2019-03-20 14:45:15 +0100

    Addd change bet feature

--- static/js/game.js
+++ static/js/game.js
@@ -2,8 +2,9 @@ function main() {
     let startButton = document.getElementById('start');
     startButton.addEventListener('click', getDealersCard);
     startButton.addEventListener('click', getPlayersCard);
+    hitCard();
+    changeBet();
 }
-
 
 
 function random() {
@@ -11,13 +12,12 @@ function random() {
                 "7C","7D","7H","7S","8C","8D","8H","8S","9C","9D","9H","9S","10C","10D","10H","10S","AC","AD","AH","AS",
                 "JC","JD","JH","JS","KC","KD","KH","KS","QC","QD","QH","QS"];
     return cards[Math.floor(Math.random() * cards.length)]
-
 }
 
 function getDealersCard() {
     let card = random();
     let dealer = document.getElementById("dealer");
-    dealer.insertAdjacentHTML("beforeend", `<div class="card"><img src="/static/img/${card}.png" height="135px" width="85px"></div>`);
+    dealer.insertAdjacentHTML("beforeend", `<div class="card"><img src="/static/img/${card}.png"></div>`);
 }
 
 function getPlayersCard() {
@@ -26,5 +26,28 @@ function getPlayersCard() {
     player.insertAdjacentHTML("beforeend", `<div class="card"><img src="/static/img/${card}.png" height="135px" width="85px"></div>`);
 }
 
+function hitCard() {
+    let hitButton = document.getElementById('hit');
+    hitButton.addEventListener('click', getPlayersCard);
+}
+
+function changeBet() {
+    let amount = 50;
+    let amountContainer = document.getElementById('betAmount');
+    let raiseButton = document.getElementById('up');
+    console.log(raiseButton)
+    let decreaseButton = document.getElementById('down');
+
+    raiseButton.addEventListener('click', function() {
+       amount += 10;
+       amountContainer.innerHTML = `Bet:<span>${amount} $</span>`;
+    })
+
+    decreaseButton.addEventListener('click', function() {
+       amount -= 10;
+       amountContainer.innerHTML = `Bet:<span>${amount} $</span>`;
+    })
+}
 
 main();
+
