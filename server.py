998e2fe0ba09c418902f598ab342350558dfb5eb
Andras Molnar <andris9016@gmail.com>
2019-03-21 08:54:15 +0100

    Unfinished home page

--- server.py
+++ server.py
@@ -8,6 +8,11 @@ def route_game():
     return render_template('game.html')
 
 
+@app.route('/index')
+def route_index():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
